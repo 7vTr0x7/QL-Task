5cd6052a64ddbb4931e33759829045d440147ec1
7vTr0x7 <7vTrox7@gmail.com>
2026-01-08 17:57:21 +0530

    fixed mutation

--- frontend/src/graphql/comment/comment.mutations.ts
+++ frontend/src/graphql/comment/comment.mutations.ts
@@ -3,7 +3,9 @@ import { COMMENT_FRAGMENT } from "./comment.fragments";
 
 export const CREATE_COMMENT = gql`
   mutation CreateComment($authorId: ID!, $postId: ID!, $text: String!) {
-    ...CommentFragment
+    createComment(authorId: $authorId, postId: $postId, text: $text) {
+      ...CommentFragment
+    }
   }
 
   ${COMMENT_FRAGMENT}
