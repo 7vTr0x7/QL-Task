4a14fb4023d608042cc595d36d5d18cf5af601f1
7vTr0x7 <7vTrox7@gmail.com>
2026-01-08 17:33:15 +0530

    post fragment

--- frontend/src/graphql/post/post.queries.ts
+++ frontend/src/graphql/post/post.queries.ts
@@ -1,25 +1,21 @@
 import { gql } from "@apollo/client";
+import { POST_FRAGMENT } from "./post.fragments";
 
 export const GET_POSTS = gql`
   query GetPosts {
     posts {
-      id
-      content
-      title
-      comments
-      author
+      ...PostFragment
     }
   }
+
+  ${POST_FRAGMENT}
 `;
 
 export const GET_POST_BY_ID = gql`
   query GetPostById($id: ID!) {
     post(id: $id) {
-      id
-      content
-      title
-      comments
-      author
+      ...PostFragment
     }
   }
+  ${POST_FRAGMENT}
 `;
